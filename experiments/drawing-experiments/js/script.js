06d8450b5aa5ef3d478662b74a87ff8c30a19eb2
Doumeki21 <noiz1991yuya@gmail.com>
2021-09-06 16:55:54 -0400

    Drawing shapes.

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -21,6 +21,20 @@ function preload() {
 Description of setup
 */
 function setup() {
+createCanvas(500, 200);
+
+background(253, 138, 200);
+
+point(250, 0);
+
+rectMode(CENTER);
+rect(250, 100, 100, 100);
+
+ellipse(250, 100, 50, 50);
+
+line(0, 0, 500, 200);
+
+line(500, 0, 0, 200);
 
 }
 
