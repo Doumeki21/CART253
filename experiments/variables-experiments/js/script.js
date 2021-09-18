8bd4997aeeb764e3516696a6c10f8936fe890262
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-09-18 12:53:47 -0400

    3.5 debugging variables

--- experiments/variables-experiments/js/script.js
+++ experiments/variables-experiments/js/script.js
@@ -10,12 +10,14 @@ Plan:
 1. Declare a variable > "let"
 **************************************************/
 
+//List varibales at the top of the code for organization!
 let bgShade = 0;
 let circleX = 0;
 let circleY = 250;
 let circleSize = 200;
 let circleSpeed = 2;
 
+//OR make special variable like this for a same unique object (like circle in this case). 
 let circle = {
   x: 0,
   y: 250,
@@ -33,10 +35,14 @@ function setup() {
 
 // draw()
 //
-// Description of draw() goes here.
+// Draw a circle in motion.
 function draw() {
 background(bgShade);
 circle.x += circle.speed;
 ellipse(circle.x, circle.y, circle.size);
 
+//Nice to print everything in console.log so it's easier to debug and connect to the orgins of errors!
+console.log(`circle.x: ${circle.x}, circle.y: ${circle.y}`);
+//alternative-
+//console.log("circleX: " + circleX);
 }
