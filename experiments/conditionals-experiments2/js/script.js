4cfc56d343512d291b50f2e9617287e1bdd072eb
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-09-23 20:06:17 -0400

    4.3 Booleans

--- experiments/conditionals-experiments2/js/script.js
+++ experiments/conditionals-experiments2/js/script.js
@@ -1,22 +1,11 @@
 /**
-Conditionals Experiment.
+Conditionals Experiment: part 2.
 Olenka Yuen
 
-IDEA FOR PROJECT 1 (rhythm game??)
-* Move mouse in 4 directions = color control.
-* keyboard = control ball movement.
-* random generation of colored squares.
-* generation of squares SLOWLY accelerates after a certain time.
+Booleans.
 */
 
-let bgShade = 0;
-let circle = {
-  x: 0,
-  y: 250,
-  size: 100,
-  speed: 1,
-};
-
+let displayCircle = false;
 
 /**
 Description of setup
@@ -30,47 +19,23 @@ function setup() {
 Description of draw()
 */
 function draw() {
-  background(bgShade);
+  background(0);
 
-  circle.x = circle.x + circle.speed;
+//USE ONLY "IF STATEMENT" IF SMTHG NEED TO HAPPEN ONLY ONCE.
+  if (mouseIsPressed) {
+    displayCircle = true;
+  }
 
-  // if (circle.x > width){
-  //   circle.speed = -circle.speed
-  // }
-  //
-  // if (circle.x < 0){
-  //   circle.speed = -circle.speed
-  // }
-  //
-  // if (mouseY < height/2) {
-  //   fill(255,0,0)
-  // }
-  // else {
-  //   fill(0,0, 255)
-  // }
+  if (displayCircle) {
+    ellipse(250, 250, 100, 100);
+  }
 
-//Color change based on 3 columns of canvas!! (Like rhythm game?)
-  // if (mouseX < width/3) {
-  //   fill(255,0,0);
-  // }
-  // else if (mouseX < 2 * width/3) {
-  //   fill(0,255,0);
-  // }
-  // else {
-  //   fill(0,0,255);
-  // }
-
-//Nested if statement for a achange in a specific location on canvas.
-fill(255, 255,255);
-
-if (circle.x > width/3) {
-  if (circle.x < 2 * width/3)
-  fill(255,0,0);
-}
-//Alternative of above code!
-// if (circle.x > width/3 && circle.x < 2 * width/3) {
-//   fill(255,0,0);
+//*"USE IF ELSE STATEMENT" IF NEED TO CLICK MULTIPLE TIMES.
+// if (keyIsPressed) {
+//   background(255);
+// }
+// else {
+//   background(0);
 // }
 
-  ellipse(circle.x, circle.y, circle.size);
 }
