732daf58b85eff0faf0c7cf389239773e5893d44
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-09-23 21:21:49 -0400

    4.5 Loops beyond counting

--- experiments/conditionals-experiments2/js/script.js
+++ experiments/conditionals-experiments2/js/script.js
@@ -3,39 +3,89 @@ Conditionals Experiment: part 2.
 Olenka Yuen
 
 Booleans.
+Loops
 */
 
-let displayCircle = false;
 
-/**
-Description of setup
-*/
+//4.4 Loops...
+// let caterpillar = {
+//   x: 100,
+//   y: 250,
+//   segmentSize: 50,
+// };
+//
+// function setup() {
+//   createCanvas (500, 500);
+//
+// }
+//
+//
+// function draw() {
+//   background (0);
+//   noStroke();
+//   fill(100, 200, 100);
+//
+//   // let x = caterpillar.x;
+//   // let numSegments = 5;
+//   // let segmentsDrawn = 0;
+//   //
+//   // while (segmentsDrawn < numSegments) {
+//   //   ellipse(x, caterpillar.y, caterpillar.segmentSize);
+//   //   x = x + 40;
+//   //   segmentsDrawn = segmentsDrawn + 1; //aka segmentsDrawn ++;
+//   // }
+//
+//   let x = caterpillar.x;
+//   let numSegments = 5;
+//
+// // for (starting factor, check counter, cause change in counter)
+//   for (let i = 0; i < numSegments; i++) {
+//     ellipse(x, caterpillar.y, caterpillar.segmentSize);
+//     x = x + 40;
+//   }
+// }
+
+
+//4.5 Loops.
+
+let circle = {
+  x: undefined,
+  y: undefined,
+  size: 100,
+};
+
+let dangerZone = {
+  x: 250,
+  y: 250,
+  size: 150,
+};
+
+
 function setup() {
   createCanvas(500, 500);
+
+  circle.x = random(0, width);
+  circle.y = random(0, height);
+
+  let d = dist(circle.x, circle.y, dangerZone.x, dangerZone.y);
+  while (d < circle.size/2 + dangerZone.size/2) {
+    circle.x = random(0, width);
+    circle.y = random(0, height);
+
+    d = dist(circle.x, circle.y, dangerZone.x, dangerZone,y);
+  }
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
   background(0);
 
-//USE ONLY "IF STATEMENT" IF SMTHG NEED TO HAPPEN ONLY ONCE.
-  if (mouseIsPressed) {
-    displayCircle = true;
-  }
+  //Danger zone.
+  noFill();
+  stroke(255, 0, 0);
+  ellipse(dangerZone.x, dangerZone.y, dangerZone.size)
 
-  if (displayCircle) {
-    ellipse(250, 250, 100, 100);
-  }
-
-//*"USE IF ELSE STATEMENT" IF NEED TO CLICK MULTIPLE TIMES.
-// if (keyIsPressed) {
-//   background(255);
-// }
-// else {
-//   background(0);
-// }
+  fill(255);
+  noStroke();
+  ellipse(circle.x, circle.y, circle.size);
 
 }
