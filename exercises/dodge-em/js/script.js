3e423057ff80169db70f5110d0e70d4417c0cfd1
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-09-26 00:02:06 -0400

    EX: dodge-em > everything works except user velocity and accel.

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -17,8 +17,15 @@ PLAN:
 let user = {
   x: 0,
   y: 250,
-  size: 50,
+  size: 100,
   fill: 0,
+
+  vx: 0,
+  vy: 0,
+  ax: 0,
+  ay: 0,
+  accelerate: 0.5,
+  maxSpeed: 5,
 
 };
 
@@ -56,22 +63,65 @@ Description of draw()
 function draw() {
     background(0);
 
+//ENEMY.
 //Enemy movement.
+enemy.x += enemy.vx;
+enemy.y += enemy.vy
+
+//Reset enemy.
 if (enemy.x > width) {
   enemy.x = 0;
-  enemy.accelerate ++
+  //enemy.accelerate ++
   enemy.y = random(0, height);
 }
 
+//User-enemy contact.
+let d = dist(user.x, user.y, enemy.x, enemy.y);
+if (d < enemy.size/2 + user.size/2) {
+  noLoop();
+}
 
+//USER.
+//User control.
+user.x = mouseX;
+user.y = mouseY;
 
-  //User.
-  user.x = mouseX;
-  user.y = mouseY;
-  //Draw user.
-  fill(user.fill);
-  stroke(255);
-  ellipse(user.x, user.y, user.size);
+// //User acceleration.
+//X direction.
+if (mouseX < user.x) {
+  user.ax = -user.accelerate;
+}
+else {
+  user.ax = user.accelerate;
+}
 
+//Y direction.
+if (mouseY < user.y) {
+  user.ay = -user.accelerate;
+}
+else {
+  user.ay = user.accelerate;
+}
+
+//User velocity.
+//Add acceleration to velocity!! + constrain velocity speed!!
+user.vx = user.vx + user.ax;
+user.vx = constrain(user.vx, -user.maxSpeed, user.maxSpeed);
+user.vy = user.vy + user.ay;
+user.vy = constrain(user.vy, -user.maxSpeed, user.maxSpeed);
+
+//Add user velocity to position.
+user.x = user.vx + user.vx;
+user.y = user.vy + user.vy;
+
+//Draw user.
+fill(user.fill);
+stroke(255);
+ellipse(user.x, user.y, user.size);
+
+//Draw enemy.
+fill(enemy.fill.r, enemy.fill.g, enemy.fill.b);
+noStroke();
+ellipse(enemy.x, enemy.y, enemy.size);
 
 }
