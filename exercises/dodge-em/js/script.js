a50654aade81cc2ca4cb33549e24c1993845a9d6
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-09-28 16:49:15 -0400

    EX2: dodge-em > finalized (comments)

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -2,7 +2,8 @@
 Exercise: Dodge-em!
 Olenka Yuen
 
-Use the mouse to dodge the objects on the screen!
+Move the mouse to control the black circle. Press on the mouse to hide from the purple enemy. The red circles are unavoidable!
+
 
 PLAN:
 *Mouse controls black circle. With velocity and acceleration.
@@ -14,6 +15,7 @@ PLAN:
 
 "use strict";
 
+//You control this avator.
 let user = {
   x: 250,
   y: 250,
@@ -28,6 +30,7 @@ let user = {
   maxSpeed: 15,
 };
 
+//Red circle enemy.
 let enemy1 = {
   x: 0,
   y: 250,
@@ -48,6 +51,7 @@ let enemy1 = {
   },
 };
 
+//Purple circle enemy.
 let enemy2 = {
   x: 0,
   y: 250,
@@ -68,18 +72,21 @@ let enemy2 = {
   },
 };
 
-let gameState = {
-  minState: 0,
-  maxState: 300,
+//Distance between user and enemies.
+let distAvatars = {
+  minDist: 0,
+  maxDist: 300,
 };
 
 let gameOverImage;
 
+//preload all assets.
 function preload() {
   gameOverImage = loadImage("assets/images/game-over.png");
 }
+
 /**
-Setup game screen!
+createCanvas and moving objects.
 */
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -89,18 +96,16 @@ function setup() {
   //enemy2
   enemy2.y = random(0, height);
   enemy2.vx = enemy2.speed;
-
+//background doesn't redraw!
   background(0);
 }
 
-
 /**
-Draw interactive characters!
+Draw interactive avatars and their coresponding functions and conditions!
 */
 function draw() {
 
-
-  //ENEMY.
+  //ENEMY1.
   //Enemy1 movement.
   enemy1.x += enemy1.vx;
   enemy1.y += enemy1.vy
@@ -108,29 +113,33 @@ function draw() {
   //Reset enemy1.
   if (enemy1.x > width) {
     enemy1.x = 0;
-    //enemy1.accelerate ++
+    //enemy1 accelerate ++
     enemy1.y = random(0, height);
   }
 
-  //User-enemy1 contact. (Game over screen.)
+  //User-enemy1 contact... Reveal Game over screen.
   let d1 = dist(user.x, user.y, enemy1.x, enemy1.y);
+  //If enemy1 touches user, game stop.
   if (d1 < enemy1.currentSize / 2 + user.size / 2) {
     imageMode(CENTER);
-    image(gameOverImage, windowWidth / 2, windowHeight / 2);
+    image(gameOverImage, width/2, height/2);
     noLoop();
   }
 
   //Enemy1 Growth state.
-  if (d1 < gameState.maxState) {
+  //If user close to enemy1, enemy1 grows.
+  if (d1 < distAvatars.maxDist) {
     if (enemy1.currentSize < enemy1.maxSize) {
       enemy1.currentSize += enemy1.growthSpeed;
     }
+    //else, enemy1 shrinks.
   } else {
     if (enemy1.currentSize > enemy1.minSize) {
       enemy1.currentSize -= enemy1.growthSpeed;
     }
   }
 
+  //ENEMY 2.
   //Enemy2 movement.
   enemy2.x += enemy2.vx;
   enemy2.y += enemy2.vy
@@ -138,13 +147,15 @@ function draw() {
   //Reset enemy2.
   if (enemy2.x > width) {
     enemy2.x = 0;
-    //enemy2.accelerate ++
+    //enemy2 accelerate ++
     enemy2.y = random(0, height);
   }
 
-  //User-enemy2 contact. (Game over screen.)
+  //If mouse is pressed, user is hidden from purple cirlce and the game continues.
   if (mouseIsPressed === false) {
+    //User-enemy2 contact... Reveal Game over screen.
     let d2 = dist(user.x, user.y, enemy2.x, enemy2.y);
+    //If user touches enemy2 while visible, game stop.
     if (d2 < enemy2.currentSize / 2 + user.size / 2) {
       imageMode(CENTER);
       image(gameOverImage, windowWidth / 2, windowHeight / 2);
@@ -152,10 +163,12 @@ function draw() {
     }
 
     //enemy2 Growth state.
-    if (d2 < gameState.maxState) {
+      //If user close to enemy2, enemy2 grows.
+    if (d2 < distAvatars.maxDist) {
       if (enemy2.currentSize < enemy2.maxSize) {
         enemy2.currentSize += enemy2.growthSpeed;
       }
+      //else, enemy2 shrinks.
     } else {
       if (enemy2.currentSize > enemy2.minSize) {
         enemy2.currentSize -= enemy2.growthSpeed;
@@ -163,22 +176,21 @@ function draw() {
     }
   }
 
-  //USER.
-  //User control.
-  // user.x = mouseX;
-  // user.y = mouseY;
-
-  // //User acceleration.
+  //User acceleration.
   //X direction.
   if (mouseX < user.x) {
+    //left side.
     user.ax = -user.accelerate;
   } else {
+    //right side.
     user.ax = user.accelerate;
   }
   //Y direction.
   if (mouseY < user.y) {
+    //left side.
     user.ay = -user.accelerate;
   } else {
+    //right side.
     user.ay = user.accelerate;
   }
 
@@ -199,12 +211,12 @@ function draw() {
     ellipse(user.x, user.y, user.size);
   }
 
-  // //Draw enemy1.
+  //Draw enemy1.
   fill(enemy1.fill.r, enemy1.fill.g, enemy1.fill.b);
   stroke(255);
   ellipse(enemy1.x, enemy1.y, enemy1.currentSize);
 
-  // //Draw enemy2.
+  //Draw enemy2.
   fill(enemy2.fill.r, enemy2.fill.g, enemy2.fill.b);
   stroke(255);
   ellipse(enemy2.x, enemy2.y, enemy2.currentSize);
