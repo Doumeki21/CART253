955a9e8b7729c36f340d16dc2306cd08e49fc948
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-01 23:20:03 -0400

    5.4 Functions text

--- experiments/functions-experiements/js/script.js
+++ experiments/functions-experiements/js/script.js
@@ -5,34 +5,40 @@ Olenka Yuen
 Vid 5.1 FUNCTIONS
 vid 5.2 w parameters
 vid 5.3 w return values
+vid 5.4 text
 */
 
 "use strict";
 
+let hello = {
+  string: `Hello, world!`,
+  x: 250,
+  y: 0,
+  vx: 1,
+  vy: 4,
+  size: 40,
+};
 
-/**
-Description of setup
-*/
 function setup() {
   createCanvas(500, 500);
 
-  let hotCelsius = toCelsius(100);
-  console.log(`100 degrees Fahrenheit is ${hotCelsius} degrees Celsius.`);
-
-  let coldCelsius = toCelsius(10);
-  console.log(`10 degrees Fahrenheit is ${coldCelsius} degrees Celsius.`);
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
   background(0);
 
-}
+  //hello.x += hello.vx;
+  hello.y += hello.vy
 
-function toCelsius(fahrenheit) {
-  let celsius = (fahrenheit - 32) * 5/9;
-  return celsius;
+  hello.size ++;
+
+  textAlign(CENTER,CENTER);
+  textStyle(BOLD);
+  textSize(hello.size);
+
+  fill(200, 50, 50);
+  stroke(255);
+  strokeWeight(4);
+
+  text(hello.string, hello.x, hello.y);
 }
