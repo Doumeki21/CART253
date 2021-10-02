c6f97b3ff7be857e837b12b6b7bc1983e7fc420d
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-01 21:31:59 -0400

    5.1 Functions

--- experiments/functions-experiements/js/script.js
+++ experiments/functions-experiements/js/script.js
@@ -1,27 +1,25 @@
 /**
-Title of Project
-Author Name
+Functions
+Olenka Yuen
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Vid 5.1 Functions
 */
 
 "use strict";
 
-
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
+let circle = {
+  x: 0,
+  y: 250,
+  size: 100,
+  vx: 1,
+  vy: 0,
+};
 
 /**
 Description of setup
 */
 function setup() {
-
+  createCanvas(500, 500);
 }
 
 
@@ -29,5 +27,36 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(0);
 
+  move();
+  wrap();
+  display();
+}
+
+function move() {
+  circle.x += circle.vx;
+  circle.y += circle.vy;
+}
+
+function wrap() {
+  if (circle.x > width) {
+    reset();
+  }
+}
+
+function display() {
+  fill(255, 0, 0);
+  ellipse(circle.x, circle.y, circle.size);
+}
+
+function reset() {
+  circle.x = 0;
+  circle.vx += 2;
+  circle.vy -= 0.25
+  circle.size += 5;
+}
+
+function mousePressed() {
+  reset();
 }
