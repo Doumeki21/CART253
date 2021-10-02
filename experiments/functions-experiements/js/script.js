497ac63200399c72dc8961c9fa819cec19b981bd
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-01 22:00:52 -0400

    5.2 parameters

--- experiments/functions-experiements/js/script.js
+++ experiments/functions-experiements/js/script.js
@@ -2,18 +2,12 @@
 Functions
 Olenka Yuen
 
-Vid 5.1 Functions
+Vid 5.1 FUNCTIONS
+vid 5.2 parameters
 */
 
 "use strict";
 
-let circle = {
-  x: 0,
-  y: 250,
-  size: 100,
-  vx: 1,
-  vy: 0,
-};
 
 /**
 Description of setup
@@ -29,34 +23,19 @@ Description of draw()
 function draw() {
   background(0);
 
-  move();
-  wrap();
-  display();
+  parallels(100, 100, 5, 1, 100, 1);
+  parallels(50,50, 10, 2, 20, 10);
+  parallels(200, 200, 15, 7, 3, 20);
+  parallels(315, 315, 20, 0.5, 300, 2);
+
 }
 
-function move() {
-  circle.x += circle.vx;
-  circle.y += circle.vy;
-}
-
-function wrap() {
-  if (circle.x > width) {
-    reset();
+function parallels(x, y, numLines, lineWidth, lineHeight, lineSpace) {
+  for (let i = 0; i < numLines; i++) {
+    noStroke();
+    fill(255);
+    rectMode(CENTER);
+    rect(x,y,lineWidth,lineHeight);
+    x += lineSpace;
   }
-}
-
-function display() {
-  fill(255, 0, 0);
-  ellipse(circle.x, circle.y, circle.size);
-}
-
-function reset() {
-  circle.x = 0;
-  circle.vx += 2;
-  circle.vy -= 0.25
-  circle.size += 5;
-}
-
-function mousePressed() {
-  reset();
 }
