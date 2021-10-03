a58e61cfd9d57f05b626a003a16396032fafec69
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-03 01:22:17 -0400

    EX03: love, actually > user input and lover jitter works!

--- exercises/03-love-actually/js/script.js
+++ exercises/03-love-actually/js/script.js
@@ -17,15 +17,15 @@ PLAN:
 let user = {
   x: 250,
   y: 250,
-  size:100,
+  size: 80,
   vx: 0,
   vy: 0,
-  speed: 3,
+  speed: 4,
 }
 let lover = {
   x: 250,
   y: 250,
-  size:100,
+  size: 80,
   vx: 0,
   vy: 0,
   speed: 6,
@@ -33,17 +33,63 @@ let lover = {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
 }
+
 
 function draw() {
   background(0);
 
-  lover.vx = random(-lover.speed, lover.speed);
-  lover.vy = random(-lover.speed, lover.speed);
+  handleInput();
+  move();
+  display();
 
+}
+
+//Move user.
+function handleInput() {
+  //User control horizontol axis.
+  if (keyIsDown(LEFT_ARROW)) {
+    user.vx = -user.speed;
+  }
+  else if (keyIsDown(RIGHT_ARROW)) {
+    user.vx = user.speed;
+  }
+  else {
+    user.vx = 0;
+  }
+  //User control vertical axis.
+  if (keyIsDown(UP_ARROW)) {
+    user.vy = -user.speed;
+  }
+  else if (keyIsDown(DOWN_ARROW)) {
+    user.vy = user.speed;
+  }
+  else {
+    user.vy = 0;
+  }
+}
+
+function move() {
+  //User movement.
+  user.x += user.vx;
+  user.y += user.vy;
+
+  //Lover movement.
   lover.x += lover.vx;
+  lover.x = constrain(lover.x, 0, width);
   lover.y += lover.vy;
+  lover.y = constrain(lover.y, 0, height);
+  //Lover jitter
+  let change = random();
+  if (change < 0.1) {
+    lover.vx = random(-lover.speed, lover.speed);
+    lover.vy = random(-lover.speed, lover.speed);
+  }
+}
 
+function display() {
+  //Display circles.
+  ellipse(user.x, user.y, user.size);
   ellipse(lover.x, lover.y, lover.size);
-
 }
