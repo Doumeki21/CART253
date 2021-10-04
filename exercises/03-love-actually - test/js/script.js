81cec7f0be2011fd013b4dd0f66b8dfe2616adbf
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-04 14:45:48 -0400

    EX03: TEST > don't play, it'll break

--- exercises/03-love-actually - test/js/script.js
+++ exercises/03-love-actually - test/js/script.js
@@ -21,3 +21,29 @@ REQUIREMENTS:
 */
 
 "use strict";
+
+// How many circles in a line to draw?
+// Start with 0 because we'll increase over time
+// for an animation!
+let numCircles = 5;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+
+  // Use a for loop to draw out circles up to the number of
+  // circles currently set in numCircles
+  for (let i = 0; i < numCircles; i--) {
+    // Draw a circle, use i to control where on the canvas
+    // we draw it so that we can draw them in a row
+    ellipse(i * 20, height/2, 20);
+  }
+
+  // Increase the number of circles to draw next time
+  // By using a fractional number we can change the animation
+  // speed a bit (one circle every ten frames in this case)
+  numCircles -= 0.01;
+}
