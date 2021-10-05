740376ea346413e5ed7f1c147de2d3ed9f5dd67e
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-04 21:53:16 -0400

    P1: wrote plan for project

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -1,9 +1,25 @@
 /**
 Title of Project
-Author Name
+Olenka Yuen
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Objectives
+  *Handling different forms of user input
+  *Organizing code with functions
+  *Making things interesting with conditionals and variables
+  *Combining aesthetics, interaction and concept
+
+Brief
+  *(Personal) Metaphorical simulation. // Mental health??
+
+Requirements
+  *At least two moving elements
+  *Interactivity
+  *Aesthetic, conceptual, and procedural harmony (visual, sound, and user-input should work harmoniously!)
+  *Beginning, middle, and end!
+
+Advice
+  *START SMALL
+  *ATOM BEAUTIFY
 */
 
 "use strict";
