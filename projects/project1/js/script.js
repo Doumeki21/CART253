958a6565e46c3e6ff20deb9180d831cea662bf15
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-04 22:37:34 -0400

    P1: drew stressball

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -27,15 +27,18 @@ Advice
 let rectBottom = {
   x: 250,
   y: 550,
-  width: 100,
-  height: 40,
+  width: 200,
+  height: 20,
 }
 
-/**
-Description of preload
-*/
-function preload() {
+let ball1 = {
+  x: 250,
+  y: 0,
+  size: 50,
 
+  vx: 0,
+  vy: 0,
+  speed: 3,
 }
 
 
@@ -44,17 +47,36 @@ Description of setup
 */
 function setup() {
   createCanvas (windowWidth, windowHeight);
+
+  setupShapes();
 }
 
+function setupShapes() {
+  ball1.vx = random(-ball1.speed, ball1.speed);
+  ball1.vy = random(-ball1.speed, ball1.speed);
+}
 
 /**
 Description of draw()
 */
 function draw() {
   background(0);
+//Move ball1
+ball1.x += ball1.vx;
+ball1.y += ball1.vy;
+//ball1 constrain in window.
+ball1.x = constrain(ball1.x, 0, width);
+ball1.y = constrain(ball1.y, 0, height);
+//Draw ball1.
+ellipse(ball1.x, ball1.y, ball1.size);
 
+//Check user-ball contact.
+
+//Bottom user.
+//Bottom user control.
   rectBottom.x = mouseX;
-
+//Display bottom user.
+  noStroke();
   rectMode(CENTER);
   rect(rectBottom.x, rectBottom.y, rectBottom.width, rectBottom.height);
 }
