17d46a2d366f5d6780514ad336aafef480937ad8
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-15 18:09:46 -0400

    7.1 intermediate functions

--- experiments/intermediate-functions-experiment/js/script.js
+++ experiments/intermediate-functions-experiment/js/script.js
@@ -8,14 +8,18 @@ author, and this description to match your project!
 
 "use strict";
 
-
-/**
-Description of preload
-*/
-function preload() {
-
+let user = {
+  x: 0,
+  y: 0,
+  size: 100,
 }
 
+let food1 = {
+  x: 250,
+  y: 300,
+  size: 50,
+  eaten: false,
+}
 
 /**
 Description of setup
