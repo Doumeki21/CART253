e3ee513a444f5db1b52b35fddb0d3bc8840747df
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-16 00:36:38 -0400

    7.3: trail works!

--- experiments/more-arrays-experiment/js/script.js
+++ experiments/more-arrays-experiment/js/script.js
@@ -8,76 +8,38 @@ Author Name
 
 "use strict";
 
-// Our fish
-let school = [];
-let schoolSize = 4;
+let circle = {
+  x: 0,
+  y: 0,
+  size: 100,
+  trail: [],
+  trailSize: 20,
+};
 
 function setup() {
   createCanvas(600, 600);
-
-  // Create four fish, positioned randomly
-  for (let i = 0; i < schoolSize; i++) {
-    let fish = createFish(random(0, width), random(0, height));
-    school.push(fish);
-  }
 }
 
-// createFish(x,y)
-// Creates a new JavaScript Object describing a fish and returns it
-function createFish(x, y) {
-  let fish = {
-    x: x,
-    y: y,
-    size: 50,
-    vx: 0,
-    vy: 0,
-    speed: 2
-  };
-  return fish;
-}
-
-// draw()
-// Moves and displays our fish
 function draw() {
   background(0);
 
-  for (let i = 0; i < school.length; i++) {
-    let fish = school[i];
-    moveFish(fish);
-    displayFish(school[i]);
-  }
+  circle.x = mouseX;
+  circle.y = mouseY;
+
+for (let i = 0; i <  circle.trail.length; i++) {
+  let position = circle.trail[i];
+  ellipse(position.x, position.y, circle.size);
 }
 
-// moveFish(fish)
-// Chooses whether the provided fish changes direction and moves it
-function moveFish(fish) {
-  // Choose whether to change direction
-  let change = random(0, 1);
-  if (change < 0.05) {
-    fish.vx = random(-fish.speed, fish.speed);
-    fish.vy = random(-fish.speed, fish.speed);
-  }
+ellipse(circle.x, circle.y, circle.size);
 
-  // Move the fish
-  fish.x = fish.x + fish.vx;
-  fish.y = fish.y + fish.vy;
+let newTrailPosition = {
+  x: circle.x,
+  y: circle.y,
+};
+circle.trail.push(newTrailPosition);
 
-  // Constrain the fish to the canvas
-  fish.x = constrain(fish.x, 0, width);
-  fish.y = constrain(fish.y, 0, height);
+if (circle.trail.length > circle.trailSize) {
+  circle.trail.shift();
 }
-
-// displayFish(fish)
-// Displays the provided fish on the canvas
-function displayFish(fish) {
-  push();
-  fill(200, 100, 100);
-  noStroke();
-  ellipse(fish.x, fish.y, fish.size);
-  pop();
-}
-
-function mousePressed() {
-  let fish = createFish(mouseX, mouseY);
-  school.push(fish);
 }
