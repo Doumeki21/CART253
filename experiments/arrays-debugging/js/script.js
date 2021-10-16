e6cc353bfd2f52cd36ed92c301dcfc6d60f7a1e0
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-16 18:18:23 -0400

    Arrays: debugging wip

---
+++ experiments/arrays-debugging/js/script.js
@@ -0,0 +1,23 @@
+/**
+Title of Project
+Author Name
+
+7.1 intermediate functions
+7.2 Introducing arrays
+*/
+
+"use strict";
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(0);
+
+  drawCircle();
+}
+
+function drawCircle( {
+  ellipse(width / 2, height / 2, 100);
+}
