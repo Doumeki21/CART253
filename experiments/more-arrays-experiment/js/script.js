57b2cdc8b9652ff637cd5d88b1919842c9d8ed54
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-16 00:46:21 -0400

    7:3, tried dog sound rates

--- experiments/more-arrays-experiment/js/script.js
+++ experiments/more-arrays-experiment/js/script.js
@@ -8,13 +8,47 @@ Author Name
 
 "use strict";
 
-let circle = {
-  x: 0,
-  y: 0,
-  size: 100,
-  trail: [],
-  trailSize: 20,
-};
+// let circle = {
+//   x: 0,
+//   y: 0,
+//   size: 100,
+//   trail: [],
+//   trailSize: 20,
+// };
+//
+// function setup() {
+//   createCanvas(600, 600);
+// }
+//
+// function draw() {
+//   background(0);
+//
+//   circle.x = mouseX;
+//   circle.y = mouseY;
+//
+//   for (let i = 0; i < circle.trail.length; i++) {
+//     let position = circle.trail[i];
+//     ellipse(position.x, position.y, circle.size);
+//   }
+//
+//   ellipse(circle.x, circle.y, circle.size);
+//
+//   let newTrailPosition = {
+//     x: circle.x,
+//     y: circle.y,
+//   };
+//   circle.trail.push(newTrailPosition);
+//
+//   if (circle.trail.length > circle.trailSize) {
+//     circle.trail.shift();
+//   }
+// }
+
+
+
+function preload() {
+
+}
 
 function setup() {
   createCanvas(600, 600);
@@ -22,24 +56,4 @@ function setup() {
 
 function draw() {
   background(0);
-
-  circle.x = mouseX;
-  circle.y = mouseY;
-
-for (let i = 0; i <  circle.trail.length; i++) {
-  let position = circle.trail[i];
-  ellipse(position.x, position.y, circle.size);
-}
-
-ellipse(circle.x, circle.y, circle.size);
-
-let newTrailPosition = {
-  x: circle.x,
-  y: circle.y,
-};
-circle.trail.push(newTrailPosition);
-
-if (circle.trail.length > circle.trailSize) {
-  circle.trail.shift();
-}
 }
