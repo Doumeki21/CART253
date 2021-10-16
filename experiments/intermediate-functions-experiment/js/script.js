3292baa70f7f301f7b9f43fed2a7fd8f7300bb22
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-15 23:56:09 -0400

    7.2: introducing more arrays

--- experiments/intermediate-functions-experiment/js/script.js
+++ experiments/intermediate-functions-experiment/js/script.js
@@ -2,36 +2,82 @@
 Title of Project
 Author Name
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+7.1 intermediate functions
+7.2 Introducing arrays
 */
 
 "use strict";
 
-let user = {
-  x: 0,
-  y: 0,
-  size: 100,
-}
+// Our fish
+let school = [];
+let schoolSize = 4;
 
-let food1 = {
-  x: 250,
-  y: 300,
-  size: 50,
-  eaten: false,
-}
-
-/**
-Description of setup
-*/
 function setup() {
+  createCanvas(600, 600);
 
+  // Create four fish, positioned randomly
+  for (let i = 0; i < schoolSize; i++) {
+    let fish = createFish(random(0, width), random(0, height));
+    school.push(fish);
+  }
 }
 
+// createFish(x,y)
+// Creates a new JavaScript Object describing a fish and returns it
+function createFish(x, y) {
+  let fish = {
+    x: x,
+    y: y,
+    size: 50,
+    vx: 0,
+    vy: 0,
+    speed: 2
+  };
+  return fish;
+}
 
-/**
-Description of draw()
-*/
+// draw()
+// Moves and displays our fish
 function draw() {
+  background(0);
 
+  for (let i = 0; i < school.length; i++) {
+    let fish = school[i];
+    moveFish(fish);
+    displayFish(school[i]);
+  }
+}
+
+// moveFish(fish)
+// Chooses whether the provided fish changes direction and moves it
+function moveFish(fish) {
+  // Choose whether to change direction
+  let change = random(0, 1);
+  if (change < 0.05) {
+    fish.vx = random(-fish.speed, fish.speed);
+    fish.vy = random(-fish.speed, fish.speed);
+  }
+
+  // Move the fish
+  fish.x = fish.x + fish.vx;
+  fish.y = fish.y + fish.vy;
+
+  // Constrain the fish to the canvas
+  fish.x = constrain(fish.x, 0, width);
+  fish.y = constrain(fish.y, 0, height);
+}
+
+// displayFish(fish)
+// Displays the provided fish on the canvas
+function displayFish(fish) {
+  push();
+  fill(200, 100, 100);
+  noStroke();
+  ellipse(fish.x, fish.y, fish.size);
+  pop();
+}
+
+function mousePressed() {
+  let fish = createFish(mouseX, mouseY);
+  school.push(fish);
 }
