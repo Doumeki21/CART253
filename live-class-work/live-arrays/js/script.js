7e6f550aacf06b93ebd1c864b058ddd79aac50c5
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-19 14:15:12 -0400

    live arrays: added coin

--- live-class-work/live-arrays/js/script.js
+++ live-class-work/live-arrays/js/script.js
@@ -15,18 +15,30 @@ let user = {
   size: 100,
 };
 
+let coin = undefined;
+
 function setup() {
   createCanvas(500 ,500);
   noCursor();
+  coin = createCoin();
+}
 
-
+function createCoin() {
+  let newCoin = {
+    x: random(0, width),
+    y: random(0, height),
+    size: 50,
+    stroke: color(255, 255, 0),
+  };
+  return newCoin;
 }
 
 function draw() {
   background(0);
 
   handleInput();
-  displayUser();
+  displayUser(user);
+  displayCoin(coin);
 }
 
 function handleInput() {
@@ -34,10 +46,18 @@ function handleInput() {
   user.y = mouseY;
 }
 
-function displayUser() {
+function displayUser(user) {
   push();
   noFill()
   stroke(255, 100, 0);
   ellipse(user.x, user.y, user.size);
+  pop();
+}
+
+function displayCoin(coin) {
+  push();
+  noFill();
+  stroke(coin.stroke);
+  ellipse(coin.x, coin.y, coin.size);
   pop();
 }
