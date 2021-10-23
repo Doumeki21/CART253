370475c128aa4af12d5c5f84c2d63703fc7bad06
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-23 01:05:49 -0400

    8.2: better use of OOP and randomization

--- experiments/oop-intro/js/Flower.js
+++ experiments/oop-intro/js/Flower.js
@@ -1,10 +1,11 @@
 class Flower {
-  constructor() {
+
+  constructor(x, y, size, stemLength, petalColor) {
     //this = the Flower it's being constructed()
-    this.x = random(0, width);
-    this.y = random(0, height);
-    this.size = 50;
-    this.stemLength = 75;
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.stemLength = stemLength;
     this.stemThickness = 10;
     this.petalThickness = 10;
 
@@ -13,11 +14,7 @@ class Flower {
       g: 150,
       b: 50,
     };
-    this.petalColor = {
-      r: 200,
-      g: 50,
-      b: 50,
-    };
+    this.petalColor = petalColor;
     this.centreColor = {
       r: 50,
       g: 0,
--- experiments/oop-intro/js/script.js
+++ experiments/oop-intro/js/script.js
@@ -10,7 +10,7 @@ author, and this description to match your project!
 
 let garden = {
   flowers: [],
-  numFlowers: 20,
+  numFlowers: 25,
   grassColor: {
     r: 120,
     g: 180,
@@ -22,8 +22,17 @@ function setup() {
   createCanvas(600, 600);
 
   for (let i = 0; i < garden.numFlowers; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    let size = random(50, 80);
+    let stemLength = random(50, 100);
+    let petalColor = {
+      r: random(100,255),
+      g: random(100,255),
+      b: random(100,255),
+    };
     //create a new flower
-    let flower = new Flower();
+    let flower = new Flower(x, y, size, stemLength, petalColor);
     //add the flower to the array of flowers.
     garden.flowers.push(flower);
   }
