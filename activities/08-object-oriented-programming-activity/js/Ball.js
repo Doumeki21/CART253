a5f87c7580c3ca0903282e7a1f81ad5a0799dd8a
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-23 16:58:04 -0400

    ACT8: FINISHED

--- activities/08-object-oriented-programming-activity/js/Ball.js
+++ activities/08-object-oriented-programming-activity/js/Ball.js
@@ -26,11 +26,23 @@ class Ball {
     //Chnaging coordinate posiiton.
     this.x += this.vx;
     this.y += this.vy;
+
+    if (this.y - this.size/2 > height) {
+      this.active = false;
+    }
   }
 
-  bounce() {
+  bounce(paddle) {
     //Chheck if abll passes bottom of canvas
-    if (this.y + this.size/2 >= height) {
+    // if (this.y + this.size/2 >= height) {
+    if (this.x > paddle.x - paddle.width / 2 &&
+      this.x < paddle.x + paddle.width / 2 &&
+      this.y + this.size / 2 > paddle.y - paddle.height / 2 &&
+      this.y - this.size / 2 < paddle.y + paddle.height / 2) {
+
+      //bounce
+      let dx = this.x - paddle.x;
+      this.vx += map(dx, -paddle.width/2, paddle.width/2, -2, 2);
       //reverse velocity
       this.vy = -this.vy;
       this.ay = 0;
