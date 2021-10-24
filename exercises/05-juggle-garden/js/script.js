c787cbc10782dbee4adf80789be26ef554d1a67b
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-23 23:02:47 -0400

    EX5: started juggle garden

---
+++ exercises/05-juggle-garden/js/script.js
@@ -0,0 +1,54 @@
+/**
+Activity 08: Object Oriented Programming Activity.
+Olenka Yuen
+
+plan
+1. defined a paddle class
+2. Set up main Programm.
+3.  define a ball class.
+4. Add balls to main program.
+5. Make balls bounce on paddle.
+*/
+
+"use strict";
+
+let gravityForce = 0.0025;
+
+let paddle;
+
+let balls = [];
+let numBalls = 20;
+
+/**
+Description of setup
+*/
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  paddle = new Paddle(300, 20);
+
+  for (let i=0; i < numBalls; i++) {
+    let x =random(0, width);
+    let y = random(-400, -100);
+    let ball = new Ball(x, y);
+    //put ball into the balls array
+    balls.push(ball);
+  }
+}
+
+function draw() {
+  background(0);
+
+  paddle.move();
+  paddle.display();
+
+  for (let i=0; i<balls.length; i++) {
+    let ball = balls[i];
+    if (ball.active) {
+      ball.gravity(gravityForce);
+      ball.move();
+      ball.bounce(paddle);
+      ball.display();
+    }
+  }
+}
