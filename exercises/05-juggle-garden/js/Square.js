5d3bd6cdb94a9b8dab87fc8241436782d3d5dae7
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-24 00:31:06 -0400

    EX5: added states

---
+++ exercises/05-juggle-garden/js/Square.js
@@ -0,0 +1,60 @@
+class Square {
+
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.vx = 0;
+    this.vy = 0;
+    this.ax = 0;
+    this.ay = 0;
+    this.maxSpeed = 15;
+    this.size = 40;
+    this.active = true;
+  }
+
+  gravity(force) {
+    this.ay += force;
+  }
+
+  move() {
+    //accelerates
+    this.vx += this.ax;
+    this.vy += this.ay;
+    //constraining the ball at reasonable speed
+    this.vx = constrain(this.vx, -this.maxSpeed, this.maxSpeed);
+    this.vy = constrain(this.vy, -this.maxSpeed, this.maxSpeed);
+    //Chnaging coordinate posiiton.
+    this.x += this.vx;
+    this.y += this.vy;
+
+    if (this.y - this.size/2 > height) {
+      this.active = false;
+    }
+  }
+
+  bounce(paddle) {
+    //Chheck if abll passes bottom of canvas
+    // if (this.y + this.size/2 >= height) {
+    if (this.x > paddle.x - paddle.width / 2 &&
+      this.x < paddle.x + paddle.width / 2 &&
+      this.y + this.size / 2 > paddle.y - paddle.height / 2 &&
+      this.y - this.size / 2 < paddle.y + paddle.height / 2) {
+
+      //bounce
+      let dx = this.x - paddle.x;
+      this.vx += map(dx, -paddle.width/2, paddle.width/2, -2, 2);
+      //reverse velocity
+      this.vy = -this.vy;
+      this.ay = 0;
+    }
+  }
+
+  display() {
+    push();
+    fill(50, 50, 255);
+    stroke(0);
+    rectMode(CENTER);
+    rect(this.x, this.y, this.size);
+    pop();
+  }
+}
--- exercises/05-juggle-garden/js/script.js
+++ exercises/05-juggle-garden/js/script.js
@@ -10,7 +10,7 @@ Brief:
 
 "use strict";
 
-let gravityForce = 0.00025;
+let ballGravityForce = 0.0025;
 
 let paddleBottom;
 let paddleTop;
@@ -18,16 +18,22 @@ let paddleTop;
 let balls = [];
 let numBalls = 15;
 
+let state = `title`;
+
 /**
 Description of setup
 */
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
+  reset();
+}
+
+function reset() {
   paddleBottom = new PaddleVertical(300, 20, height - 30);
   paddleTop = new PaddleVertical(300, 20, 30)
 
-  for (let i=0; i < numBalls; i++) {
+  for (let i = 0; i < numBalls; i++) {
     let x = random(0, width);
     let y = random(-400, -100);
     let ball = new Ball(x, y);
@@ -39,20 +45,91 @@ function setup() {
 function draw() {
   background(0);
 
+  //Screen title.
+  if (state === `title`) {
+    title();
+  }
+  //Instructions
+  else if (state === `instructions`) {
+    instructions();
+  }
+  //game
+  else if (state === `game`) {
+    game();
+  }
+}
+
+function title() {
+  //title
+  push();
+  textSize(100);
+  fill(200, 100, 100);
+  textAlign(CENTER, CENTER);
+  text(`CONTAIN`, width / 2, height / 2);
+  pop();
+
+  //Next step
+  push();
+  textSize(20);
+  fill(212, 212, 212);
+  textAlign(CENTER, CENTER);
+  text(`CLICK TO CONTINUE`, width / 2, height - 100);
+  pop();
+}
+
+function instructions() {
+  push();
+  textSize(30);
+  fill(200, 100, 100);
+  textAlign(CENTER, CENTER);
+  text(`STAY ON TASK WITH YOUR WORK BY`, width / 2, height / 2);
+  pop();
+
+  push();
+  textSize(30);
+  fill(200, 100, 100);
+  textAlign(CENTER, CENTER);
+  text(`MOVING THE TOP/ BOTTOM PADDLES WITH YOUR MOUSE AND\n PRESSING "W" AND "S" TO MOVE THE SIDE PADDLES.`, width / 2, height / 2 + 60);
+  pop();
+
+  push();
+  textSize(30);
+  fill(200, 100, 100);
+  textAlign(CENTER, CENTER);
+  text(`CAREFUL NOT TO GET DISTRACTED!`, width / 2, height / 2 + 200);
+  pop();
+
+  push();
+  textSize(20);
+  fill(212, 212, 212);
+  textAlign(CENTER, CENTER);
+  text(`CLICK TO CONTINUE`, width / 2, height - 100);
+  pop();
+}
+
+function game() {
   paddleTop.move();
   paddleTop.display();
 
   paddleBottom.move();
   paddleBottom.display();
 
-  for (let i=0; i<balls.length; i++) {
+  for (let i = 0; i < balls.length; i++) {
     let ball = balls[i];
     if (ball.active) {
-      ball.gravity(gravityForce);
+      ball.gravity(ballGravityForce);
       ball.move();
       ball.bounce(paddleTop);
       ball.bounce(paddleBottom);
       ball.display();
     }
+  }
+}
+
+function mouseClicked() {
+  if (state === `title`) {
+    state = `instructions`;
+  } else if (state === `instructions`) {
+    state = `game`;
   }
 }
