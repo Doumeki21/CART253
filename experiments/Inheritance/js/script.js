6df69ac7f9e41bc31409ed01ec5a9e7a82f4d6ec
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-29 16:22:16 -0400

    9.3 polymorphism

--- experiments/Inheritance/js/script.js
+++ experiments/Inheritance/js/script.js
@@ -8,48 +8,39 @@ author, and this description to match your project!
 
 "use strict";
 
-let cars = [];
+let vehicles = [];
 let numCars = 10;
-
-let motorcycles = [];
+// let motorcycles = [];
 let numMotorcycles = 10;
 
 function setup() {
   createCanvas(600, 600);
 
-  //create cars in the array.
+  //create cars in the (vehicles) array.
   for(let i = 0; i < numCars; i++) {
     let x = random(0, width);
     let y = random(0, height);
     let car = new Car(x, y);
-    cars.push(car);
+    vehicles.push(car);
   }
 
-  //create motorcycle in the array.
+  //create motorcycle in the (vehicles) array.
   for(let i = 0; i < numMotorcycles; i++) {
     let x = random(0, width);
     let y = random(0, height);
     let motorcycle = new Motorcycle(x, y);
-    motorcycles.push(motorcycle);
+    vehicles.push(motorcycle);
   }
 }
 
 function draw() {
   background(0);
 
-  for (let i = 0; i < cars.length; i++) {
-    //give me the current car in the index.
-    let car = cars[i];
-    car.move();
-    car.wrap();
-    car.display();
-  }
-
-  for (let i = 0; i < motorcycles.length; i++) {
-    //give me the current motorcycle in the index.
-    let motorcycle = motorcycles[i];
-    motorcycle.move();
-    motorcycle.wrap();
-    motorcycle.display();
+  for (let i = 0; i < vehicles.length; i++) {
+    //give me the current vehicle in the index.
+    let vehicle = vehicles[i];
+    vehicle.move();
+    vehicle.wrap();
+    vehicle.display();
   }
 }
