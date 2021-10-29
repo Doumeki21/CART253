d7c14c54174a7b9bee6a072d4596349d1c5cef3c
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-29 17:27:27 -0400

    ACT9: setting up pedestrian constructor, script

---
+++ activities/09-inheritance-activity/js/Pedestrian.js
@@ -0,0 +1,11 @@
+class Pedestrian {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = 20;
+    this.vx = 0;
+    this.vy = 0;
+    this.speed = 5;
+    this.alive = true;
+  }
+}
--- activities/09-inheritance-activity/js/script.js
+++ activities/09-inheritance-activity/js/script.js
@@ -1,33 +1,70 @@
 /**
-Title of Project
-Author Name
+Activity 9: inheritance Activity
+Olenka Yuen
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+PLAN:
+
+  -Write a main script with states
+  -Create a basic Pedestrian class and integrate it into the main script
+  -Create a Vehicle class
+  -Create Car, Truck, and Motorcycle classes that extend the Vehicle class
+  -Add cars, trucks, and motorcycles to the main script
+  -Add success and failure conditions
+
 */
 
 "use strict";
 
+let state = `title`;
 
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
-Description of setup
-*/
 function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+
+  if(state === `title`) {
+    title();
+  }
+  else if (state === `simulation`) {
+    simulation();
+  }
+  else if (state === `success`) {
+    success();
+  }
+  else if (state === `dead`) {
+    dead();
+  }
+}
+
+function title() {
+  displayText(`PEDESTRIAN PALAVER`);
+}
+
+function simulation() {
 
 }
 
+function success() {
+  displayText(`YOU MADE IT!`);
+}
 
-/**
-Description of draw()
-*/
-function draw() {
+function dead() {
+  displayText(`YOU DIED`);
+}
 
+function displayText(string) {
+  push();
+  textAlign(CENTER, CENTER);
+  textSize(32);
+  fill(255);
+  text(string, width/2, height/2);
+  pop();
+}
+
+function keyPressed() {
+  if (state === `title`) {
+    state = `simulation`;
+  }
 }
