c8f48fb8d7ea45955daf6f1eefae7271e987e266
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-29 16:02:25 -0400

    9.2 overriding methods

--- experiments/Inheritance/js/Car.js
+++ experiments/Inheritance/js/Car.js
@@ -4,6 +4,34 @@ class Car extends Vehicle {
     this.width = 50;
     this.height = 20;
     this.vx = 5;
+    this.drunk = 0.2;
+  }
+
+  //we overwritten the super class move(),
+  move() {
+    this.veer();
+    //So we need to recall the Vehicle
+    super.move();
+  }
+
+  veer() {
+    let r = random();
+    if (r < this.drunk) {
+      this.vy = random(-5, 5);
+    }
+  }
+
+//Also overwritten super class wrap(),
+  wrap() {
+    //so we recall super wrap().
+    super.wrap();
+//and have cars reappear on opposite side side after go off screen
+    if (this.y > height) {
+      this.y -= height;
+    }
+    else if (this.y < 0) {
+      this.y += height;
+    }
   }
 
   display() {
