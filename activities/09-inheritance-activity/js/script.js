fd41e48ba3e1602ce437b1d7d7c1d80bf5ccd369
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-29 21:39:58 -0400

    ACT9: vehicles and user displays on screen

--- activities/09-inheritance-activity/js/script.js
+++ activities/09-inheritance-activity/js/script.js
@@ -18,19 +18,58 @@ PLAN:
 let state = `simulation`;
 
 let pedestrian;
+let vehicles = [];
+let numCars = 10;
+let numTrucks = 10;
+let numMotorcycles = 5;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  let x = width/2;
+  let x = width / 2;
   let y = height;
   pedestrian = new Pedestrian(x, y);
+
+  for (let i = 0; i < numCars; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    let car = new Car(x, y);
+    vehicles.push(car);
+  }
+
+  for (let i = 0; i < numTrucks; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    let truck = new Truck(x, y);
+    vehicles.push(truck);
+  }
+
+  for (let i = 0; i < numMotorcycles; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    let motorcycle = new Motorcycle(x, y);
+    vehicles.push(motorcycle);
+  }
+
+  //Set vehicles at 50% random directions
+  for (let i = 0; i < vehicles.length; i++) {
+    let vehicle = vehicles[i];
+    let r = random(0, 1);
+    if (r < 0.5) {
+      //Go left.
+      vehicle.vx = -vehicle.speed;
+    }
+    else {
+      //Go right.
+      vehicle.vx = vehicle.speed;
+    }
+  }
 }
 
 function draw() {
   background(0);
 
-  if(state === `title`) {
+  if (state === `title`) {
     title();
   }
   else if (state === `simulation`) {
@@ -53,6 +92,12 @@ function simulation() {
   pedestrian.move();
   pedestrian.display();
 
+  for (let i = 0; i < vehicles.length; i++) {
+    let vehicle = vehicles[i];
+    vehicle.move();
+    vehicle.wrap();
+    vehicle.display();
+  }
 }
 
 function success() {
@@ -68,7 +113,7 @@ function displayText(string) {
   textAlign(CENTER, CENTER);
   textSize(32);
   fill(255);
-  text(string, width/2, height/2);
+  text(string, width / 2, height / 2);
   pop();
 }
 
