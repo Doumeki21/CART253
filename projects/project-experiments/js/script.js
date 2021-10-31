fb193d5599436fd0045edac646e07b0e21ce8ef7
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-10-31 01:27:08 -0400

    drew circle, can't figure how to restrict green dot

--- projects/project-experiments/js/script.js
+++ projects/project-experiments/js/script.js
@@ -24,71 +24,80 @@ Advice
 
 "use strict";
 
-let monologue = [
-  `I might fail.`,
-  `...`,
-  `failure is not an option.`,
-];
+// let monologue = [
+//   `I might fail.`,
+//   `...`,
+//   `failure is not an option.`,
+// ];
+//
+// let currentIndex = 0;
+// let timer;
+//
+// function setup() {
+//   createCanvas(400, 400);
+//   timer = 3000;
+//
+//   fill(255);
+//   textSize(32);
+//   textAlign(CENTER, CENTER);
+// }
+//
+// function draw() {
+//   background(100);
+//
+//   text(monologue[currentIndex], width/2, height/2);
+//
+// if (millis() > timer) {
+//   timer += timer;
+//   currentIndex ++;
+//   }
+// }
 
-let currentIndex = 0;
-let timer;
-
-function setup() {
-  createCanvas(400, 400);
-  timer = 3000;
-
-  fill(255);
-  textSize(32);
-  textAlign(CENTER, CENTER);
+let targets = [];
+let numTargets = 1;
+let target = {
+  x: 0,
+  y: 0,
+  size: 50,
 }
 
-function draw() {
-  background(100);
+function setup() {
+  createCanvas(600, 600);
+  angleMode(DEGREES);
 
-  text(monologue[currentIndex], width/2, height/2);
-
-if (millis() > timer) {
-  timer += timer;
-  currentIndex ++;
+  for (let i = 0; i < numTargets; i++) {
+     target.x = random(0, 360);
+     target.y = random(0, 360);
+    targets.push(target);
   }
 }
 
-// //Canvas change sizes and dimensions at some point of the game.
-// //Canvas shrinks height from line 8 to line 13.
-// function canvasChange() {
-//   if (currentIndex >= 7 && currentIndex < 11) {
-//     //Window height decreases-
-//     currentWindow.y -= 1;
-//     //Until it reaches to half the window's original height.
-//     currentWindow.y = constrain(currentWindow.y, windowHeight / 2, windowHeight);
-//     resizeCanvas(windowWidth, currentWindow.y);
-//     //Bottom user is moved proportionally to the current window size.
-//     rectBottom.y = currentWindow.y - 30;
-//   } else if (currentIndex >= 11 && currentIndex < 13) {
-//     //Window height increases to initial height while window width decreases-
-//     currentWindow.y += 2;
-//     currentWindow.x -= 2;
-//     //Until it reaches to half the window width.
-//     currentWindow.y = constrain(currentWindow.y, windowHeight / 2, windowHeight);
-//     currentWindow.x = constrain(currentWindow.x, windowWidth / 3, windowWidth);
-//     resizeCanvas(currentWindow.x, currentWindow.y);
-//     //Bottom user is moved proportionally to the current window size.
-//     rectBottom.y = currentWindow.y - 30;
-//   }
-//   //Canvas shrinks into thirds of the screen from line 13 until the last line.
-//   else if (currentIndex >= 13) {
-//     //Diemnsions of the canvas shrinks by 1 every frame-
-//     currentWindow.y -= 1;
-//     currentWindow.x -= 1;
-//     //Until it reaches to a third of the window's original dimensions.
-//     currentWindow.y = constrain(currentWindow.y, windowHeight / 3, windowHeight);
-//     currentWindow.x = constrain(currentWindow.x, windowWidth / 3, windowWidth);
-//     resizeCanvas(currentWindow.x, currentWindow.y);
-//     //Bottom user is moved proportionally to the current window size.
-//     rectBottom.y = currentWindow.y - 30;
-//   }
-//   //Else canvas returns to the window max size.
-//   else {
-//     resizeCanvas(windowWidth, windowHeight);
-//   }
-// }
+function draw() {
+  background(0);
+
+  let hr = hour();
+  let min = minute();
+  let sec = second();
+
+//White stroke
+  strokeWeight(20);
+  stroke(255);
+  noFill()
+  ellipse(300, 300, 300);
+
+//red stroke
+  strokeWeight(10);
+  stroke(255, 100, 100);
+  let end = map(mouseX, 0, width, 0, 360);
+  arc(300, 300, 300, 300, 0, end);
+
+//target
+  fill(0, 255, 0);
+  noStroke();
+  ellipse(target.x, target.y, target.size);
+
+  // fill(255);
+  // noStroke();
+  // textSize(32);
+  // text(hr + `:` + min + `:` + sec, 10, 200);
+}
