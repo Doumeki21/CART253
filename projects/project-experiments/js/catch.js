29f2bf4ee6208b5d7a63c1c12a290e23b2b13dd4
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-11-02 15:53:21 -0400

    ball goes down

--- projects/project-experiments/js/catch.js
+++ projects/project-experiments/js/catch.js
@@ -3,20 +3,44 @@
 let user = {
   x: undefined,
   y: undefined,
-  width: 100,
-  height: 20,
+  width: 50,
+  height: 30,
+}
+
+let ball = {
+  x: undefined,
+  y: 0,
+  vy: 8,
+  size: 20,
 }
 
 function setup() {
   createCanvas(800, 800);
 
-   user.x = width/2;
-   user.y = height - 30;
+  ball.x = random(5, width - 30);
+
+  user.x = width / 2;
+  user.y = height - 30;
+
+  line()
 }
 
 function draw() {
   background(0);
 
+  //Ball goes down
+  ball.y += ball.vy;
+
+
+//user grows taller
+  if (ball.y + ball.size / 2 > user.height - user.size / 2) {
+    user.height += ball.size / 2;
+  }
+
+  //target
+  ellipse(ball.x, ball.y, ball.size);
+
+  //User
   user.x = mouseX;
   rectMode(CENTER);
   rect(user.x, user.y, user.width, user.height);
