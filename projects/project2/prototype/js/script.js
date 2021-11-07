e2c2d283fa65bef125956b6db918ba77a6219579
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-11-06 21:39:21 -0400

    PROTO: drew the meter and fillmeter

--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -1,6 +1,6 @@
 /**
-Title of Project
-Author Name
+Project 2: prototype
+Olenka Yuen
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
@@ -8,19 +8,27 @@ author, and this description to match your project!
 
 "use strict";
 
-
-/**
-Description of preload
-*/
-function preload() {
-
+let meter = {
+  x: 300,
+  y: 300,
+  size: 300,
 }
+
+let fillMeter = {
+  x: 300,
+  y: 300,
+  width: 300,
+  height: 300,
+}
+
 
 
 /**
 Description of setup
 */
 function setup() {
+  createCanvas(600, 600);
+  angleMode(DEGREES);
 
 }
 
@@ -29,5 +37,21 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(0, 0, 70);
+
+//White stroke
+strokeWeight(20);
+stroke(255);
+noFill()
+ellipse(meter.x, meter.y, meter.size);
+
+//red stroke/ fillMeter
+push();
+strokeWeight(10);
+stroke(255, 0, 43);
+let mouseEnd = map(mouseX, 0, width, 0, 360);
+arc(fillMeter.x, fillMeter.y, fillMeter.width, fillMeter.height, 0, mouseEnd);
+pop();
+
 
 }
