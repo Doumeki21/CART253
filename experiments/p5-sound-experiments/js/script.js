9721ef2ff7108dbd5e01e7b8533eb9dc50cc338c
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-11-13 17:45:23 -0500

    10.1 finished sounds (P2 idea)

--- experiments/p5-sound-experiments/js/script.js
+++ experiments/p5-sound-experiments/js/script.js
@@ -10,30 +10,25 @@ author, and this description to match your project!
 
 let barkSFX;
 
-/**
-Description of preload
-*/
 function preload() {
   barkSFX = loadSound(`assets/sounds/bark.wav`);
 
 }
 
 
-/**
-Description of setup
-*/
 function setup() {
   createCanvas(600, 600);
+  userStartAudio();
 }
 
 
-/**
-Description of draw()
-*/
 function draw() {
   background(0);
+
+  let newRate = map(mouseX, 0, width, -3, 3);
+  barkSFX.rate(newRate);
 }
 
 function mouseClicked() {
-  barkSFX.play();
+  barkSFX.loop();
 }
