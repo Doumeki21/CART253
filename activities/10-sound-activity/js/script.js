d2df04b3706346c2e23c4bcc145af78f726697cb
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-11-14 00:17:30 -0500

    ACT10: done

--- activities/10-sound-activity/js/script.js
+++ activities/10-sound-activity/js/script.js
@@ -1,33 +1,48 @@
 /**
-Title of Project
-Author Name
+Activity 10: Musical toy
+Olenka Yuen
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+A program that plays music based on primitive physics.
+
+PLAN:
+    - Write a basic Ball class
+    - Integrate the class into the main script
+    - Add an oscillator to the Ball class
+    - Add a synthesizer to the Ball class
 */
 
 "use strict";
 
+let balls = [];
 
-/**
-Description of preload
-*/
-function preload() {
+//F-minor
+let notes = [`F3`, `G3`, `Ab4`, `Bb4`, `C4`, `Db4`, `Eb4`, `F4`];
 
-}
-
-
-/**
-Description of setup
-*/
+// Draw the canvas.
 function setup() {
+  createCanvas(600, 600);
 
+  userStartAudio();
 }
 
-
-/**
-Description of draw()
-*/
+//
 function draw() {
+  background(0);
 
+  for (let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    ball.move();
+    ball.bounce();
+    ball.display();
+  }
+}
+
+function mousePressed() {
+  createBall(mouseX, mouseY);
+}
+
+function createBall(x, y) {
+  let note = random(notes)
+  let ball = new Ball(x, y, note);
+  balls.push(ball);
 }
