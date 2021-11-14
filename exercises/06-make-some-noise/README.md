31794f234fc6e4b49fca07fd78febbc162ecc739
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-11-14 15:06:07 -0500

    EX6: finished

--- exercises/06-make-some-noise/README.md
+++ exercises/06-make-some-noise/README.md
@@ -1,3 +1,6 @@
 # Exercise 06: Make Some Noise
 
 Producing a prototype for the sound aspect of the final project.
+
+Goal:
+Put the ball inside the hoop to increase the progress bar!
--- exercises/06-make-some-noise/js/script.js
+++ exercises/06-make-some-noise/js/script.js
@@ -7,7 +7,7 @@ Olenka Yuen
 Producing a prototype for the sound aspect of the final project.
 
 Goal:
-
+Put the ball inside the hoop to increase the progress bar!
 **************************************************/
 
 let barkSFX;
@@ -31,7 +31,6 @@ let progressBar = {
   width: 10,
   minHeight: 20,
   maxHeight: 200,
-  edgeHeight: undefined,
   currentHeight: undefined,
 };
 
@@ -48,50 +47,61 @@ function preload() {
   barkSFX = loadSound(`assets/sounds/bark.wav`);
 }
 
+//set initial state of elements.
 function setup() {
   createCanvas(600, 600);
   userStartAudio();
   angleMode(DEGREES);
 
+  //Hoop placed near the right side of the canvas.
   hoop.x = width - 100;
   hoop.y = height / 2 - 100;
 
+  //the base of the progressBar placed near the bottom left corner
   meter.x = 30;
   meter.y = height - 50;
 
+  //the progressBar placed near the bottom left corner
   progressBar.x = 30;
   progressBar.y = height - 50;
 
-  progressBar.edgeHeight = 0;
+  //progress bar begins at its minimum height.
   progressBar.currentHeight = progressBar.minHeight;
 }
 
 function draw() {
+  //Pink background.
   background(255, 200, 200);
+
+//Sound pitch raises as it fills the bar.
   let newRate = map(progressBar.currentHeight, progressBar.minHeight, progressBar.maxHeight, 1, 8);
   barkSFX.rate(newRate);
 
+//mouse controls the ball.
   ball.x = mouseX;
   ball.y = mouseY;
 
-  //Check ball inside hoop
+  //Check if ball is inside hoop
   if (ball.x + ball.size / 2 > hoop.x &&
     ball.x - ball.size / 2 < hoop.x &&
     ball.y + ball.size / 2 > hoop.y &&
     ball.y - ball.size / 2 < hoop.y) {
-    //score point!
+    //if it is, scores point once.
     if (scoreCanChange === true) {
       progressBar.currentHeight += 20;
       barkSFX.play();
       scoreCanChange = false;
     }
   }
+  //Once ball is outside the hoop, it can score again.
   else {
     scoreCanChange = true;
   }
 
-//Check bar increase.
+  //Check bar fill amount.
+  //If bar reaches maximum meter height,
   if (progressBar.currentHeight >= progressBar.maxHeight) {
+    //print this text.
     push();
     fill(64, 123, 167);
     strokeWeight(30);
@@ -99,23 +109,25 @@ function draw() {
     text(`YOU DID IT!`, 100, 100);
     pop();
   }
-  if (progressBar.y - progressBar.currentHeight/2 < 0) {
+  //If ball reaches canvas edge,
+  if (progressBar.y - progressBar.currentHeight < 0) {
+    //print this text.
     push();
     fill(64, 123, 167);
     strokeWeight(30);
     textSize(30);
-    text(`OUT OF THIS WORLD!`, 100, 100);
+    text(`OUT OF THIS WORLD!`, 100, 150);
     pop();
   }
 
-  //ball
+  //Display ball
   push();
   fill(64, 123, 167);
   noStroke();
   ellipse(ball.x, ball.y, ball.size);
   pop();
 
-  //hoop
+  //Display hoop
   push();
   noFill();
   strokeWeight(5);
@@ -123,7 +135,7 @@ function draw() {
   arc(hoop.x, hoop.y, hoop.width, hoop.height, 0, 360);
   pop();
 
-  //meter
+  //Display meter
   push();
   noStroke();
   fill(255, 200);
@@ -131,7 +143,7 @@ function draw() {
   rect(meter.x, meter.y - meter.height / 2, meter.width, meter.height);
   pop();
 
-  //progressBar
+  //Display progressBar
   push();
   noStroke();
   fill(224, 100, 100);
