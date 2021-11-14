2cd9562b4a01e721201752207bbaf2a35db85b25
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-11-13 22:22:41 -0500

    10.3 Polysynth

--- experiments/p5-polysynth-experiments/js/script.js
+++ experiments/p5-polysynth-experiments/js/script.js
@@ -8,84 +8,35 @@ author, and this description to match your project!
 
 "use strict";
 
-//10.1
-// let barkSFX;
-//
-// function preload() {
-//   barkSFX = loadSound(`assets/sounds/bark.wav`);
-//
-// }
-//
-// function setup() {
-//   createCanvas(600, 600);
-//   userStartAudio();
-// }
-//
-// function draw() {
-//   background(0);
-//
-// //Use rate for raising pitch of progress bar for p2!
-//   let newRate = map(mouseX, 0, width, -3, 3);
-//   barkSFX.rate(newRate);
-// }
-//
-// function mouseClicked() {
-//   barkSFX.loop();
-// }
-
-
-//10.2
-
-let oscillator;
-let angle = 0;
-let t = 0;
+let synth;
+let notes = [`F4`, `G4`, `Ab4`, `C4`, `Db4`, `Eb4`, `F5`];
+let currentNote = 0;
 
 function setup() {
   createCanvas(600, 600);
-  userStartAudio();
 
-  oscillator = new p5.Oscillator(200, `sine`);
-  oscillator.amp(0.2);
+  synth = new p5.PolySynth();
+
+  userStartAudio();
 }
 
 function draw() {
   background(0);
-
-// //fluctation
-//   let newFreq = map(mouseY, height, 0, 0, 800);
-//   oscillator.freq(newFreq);
-//
-// //volume
-//   let newAmp = map(mouseX, 0, width, 0, 1);
-//   oscillator.amp(newAmp);
-
-// //Alien sound?
-// let sinAngle = sin(angle); //tan(angle) is like backwards, jumping quick high to low?
-// let newFreq = map(sinAngle, -1, 1, 440, 800); //440, 800 or 0, 2000
-
-let noiseValue = noise(t);
-let newFreq = map(noiseValue, 0, 1, 440, 800);
-oscillator.freq(newFreq);
-
-t += 0.1;
-// angle += 0.2; //0.2
-
-//fast alien sounds?
-let r = random(0,1);
-
-//Visualize sound!
-// push();
-// textSize(32);
-// textAlign(LEFT, CENTER);
-// fill(255);
-// text(newFreq, 100, height/2);
-// pop();
 }
 
-function mousePressed() {
-  oscillator.start();
+function keyPressed() {
+  //500 ms
+  setInterval(playRandomNote, 200);
 }
 
-function mouseReleased() {
-  oscillator.stop();
+function playRandomNote() {
+  let note = notes[currentNote]
+  //NOTE, VELOCITY, NO DELAY, SUSAIN 1 SEC
+  synth.play(note, 1, 0, 0.25);
+
+  currentNote ++;
+
+  if (currentNote === notes.length) {
+    currentNote =0;
+  }
 }
