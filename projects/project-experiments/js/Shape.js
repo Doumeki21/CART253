a1b04b6a0eec0b43bae4b967555b5e868d6830e2
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-11-16 18:16:34 -0500

    project-exp: separated element into classes, all shapes draggable

---
+++ projects/project-experiments/js/Shape.js
@@ -0,0 +1,53 @@
+class Shape {
+  constructor(x, y, shapeColor, shapeType) {
+    this.x = x;
+    this.y = y;
+    this.size = 80;
+    this.color = shapeColor;
+    this.shapeType = shapeType;
+    this.isBeingDragged = false;
+  }
+
+  mouseInsideObject() {
+    //Get dist b/w mouse and shape
+    let d = dist(mouseX, mouseY, this.x, this.y);
+    if (d < this.size / 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  objectInsideField() {
+    //Get dist b/w object and field
+    let d = dist(this.x, this.y, field.x, field.y);
+    if (d < field.size / 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  drag() {
+    if (this.isBeingDragged) {
+      //Drag object
+      this.x = mouseX + this.offsetX;
+      this.x = constrain(mouseX, 0, width);
+      this.y = mouseY + this.offsetY;
+      this.y = constrain(mouseY, 0, height);
+    }
+  }
+
+  display() {
+    push();
+    fill(this.color);
+    stroke(255);
+    if (this.shapeType === `circle`) {
+      ellipse(this.x, this.y, this.size);
+    } else if (this.shapeType === `square`) {
+      rectMode(CENTER);
+      rect(this.x, this.y, this.size, this.size);
+    }
+    pop();
+  }
+}
--- projects/project-experiments/js/drag-drop.js
+++ projects/project-experiments/js/drag-drop.js
@@ -1,66 +1,12 @@
 "use strict";
 
-let redCircle = {
-  x: undefined,
-  y: undefined,
-  isBeingDragged: false,
-  size: 80,
-  fill: {
-    r: 255,
-    g: 0,
-    b: 0,
-  },
-};
-
-let greenCircle = {
-  x: undefined,
-  y: undefined,
-  isBeingDragged: false,
-  size: 80,
-  fill: {
-    r: 0,
-    g: 255,
-    b: 0,
-  },
-};
-
-let redSquare = {
-  x: undefined,
-  y: undefined,
-  isBeingDragged: false,
-  size: 80,
-  fill: {
-    r: 255,
-    g: 0,
-    b: 0,
-  },
-};
-
-let greenSquare = {
-  x: undefined,
-  y: undefined,
-  isBeingDragged: false,
-  size: 80,
-  fill: {
-    r: 0,
-    g: 255,
-    b: 0,
-  },
-};
-
 let field = {
   x: undefined,
   y: undefined,
   size: 500,
 };
 
-let tasks = [`redCircle`, `redSquare`, `greenCircle`, `greenSquare`];
-let maxTask = 2;
-let yourTask = `Your task`;
-
-// At the top somewhere
-let currentTasks = [];
-
+let shapes = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -69,38 +15,42 @@ function setup() {
 }
 
 function reset() {
-  //reset array.
-  let tasks = [];
 
-//Field is at the center, under the title
+  //Field is at the center, under the title
   field.x = width / 2;
   field.y = height / 2 + 50;
 
-//Place the shapes in a column
-  redCircle.y = height / 2 - 140;
-  greenCircle.y = height / 2 - 10;
-  redSquare.y = height / 2 + 120;
-  greenSquare.y = height / 2 + 250;
+  shapes[0] = new Shape(field.x + field.size / 2 + 100, height / 2 - 140, color(255, 0, 0), `circle`);
+
+  shapes[1] = new Shape(field.x + field.size / 2 + 100, height / 2 - 10, color(0, 255, 0), `circle`);
+
+  shapes[2] = new Shape(field.x + field.size / 2 + 100, height / 2 + 120, color(255, 0, 0), `square`);
+
+  shapes[3] = new Shape(field.x + field.size / 2 + 100, height / 2 + 250, color(0, 255, 0), `square`);
+
+
+  // //Place the shapes in a column
+  // redCircle.x = field.x + field.size / 2 + 100;
+  // redCircle.y = height / 2 - 140;
+  //
+  // greenCircle.y = height / 2 - 10;
+  // redSquare.y = height / 2 + 120;
+  // greenSquare.y = height / 2 + 250;
 }
 
 function draw() {
   background(0);
 
-  // for (let i = 0; i < maxTask; i++) {
-  //   yourTask = random(tasks)
+  // // And then when you're selecting them
+  // let firstTask = random(tasks);
+  // currentTasks.push(firstTask); // Choose one random task
+  // let secondTask = random(tasks); // Choose a second random task
+  // // Check if the second task is the same as the first and re-choose until it's not
+  // while (secondTask === firstTask) {
+  //   secondTask = random(tasks); // Choose a different random task
   // }
-
-  // And then when you're selecting them
-  let firstTask = random(tasks);
-  currentTasks.push(firstTask); // Choose one random task
-  let secondTask = random(tasks); // Choose a second random task
-  // Check if the second task is the same as the first and re-choose until it's not
-  while (secondTask === firstTask) {
-    secondTask = random(tasks); // Choose a different random task
-  }
-  curentTasks.push(secondTask);
-  // Now you have an array called currentTasks with two different random tasks in it
-
+  // curentTasks.push(secondTask);
+  // // Now you have an array called currentTasks with two different random tasks in it
 
   //TITLE
   push();
@@ -119,34 +69,94 @@ function draw() {
   rect(field.x, field.y, field.size);
   pop();
 
-  //shapes
-  //redCircle
-  push();
-  noStroke();
-  fill(redCircle.fill.r, redCircle.fill.g, redCircle.fill.b);
-  ellipse(field.x + field.size / 2 + 100, redCircle.y, redCircle.size);
-  pop();
+  // Shapes
+  for (let i = 0; i < shapes.length; i++) {
+    let shape = shapes[i];
+    shape.display();
+    shape.mouseInsideObject();
+    shape.drag();
+    shape.objectInsideField();
+    //
+    // console.log(shape);
+  }
 
-  //greenCircle
-  push();
-  noStroke();
-  fill(greenCircle.fill.r, greenCircle.fill.g, greenCircle.fill.b);
-  ellipse(field.x + field.size / 2 + 100, greenCircle.y, greenCircle.size);
-  pop();
+  // //redCircle
+  // push();
+  // stroke(255);
+  // fill(redCircle.fill.r, redCircle.fill.g, redCircle.fill.b);
+  // ellipse(redCircle.x, redCircle.y, redCircle.size);
+  // pop();
+  //
+  // //greenCircle
+  // push();
+  // stroke(255);
+  // fill(greenCircle.fill.r, greenCircle.fill.g, greenCircle.fill.b);
+  // ellipse(field.x + field.size / 2 + 100, greenCircle.y, greenCircle.size);
+  // pop();
+  //
+  // //redSquare
+  // push();
+  // stroke(255);
+  // rectMode(CENTER);
+  // fill(redSquare.fill.r, redSquare.fill.g, redSquare.fill.b);
+  // rect(field.x + field.size / 2 + 100, redSquare.y, redSquare.size);
+  // pop();
+  //
+  // //greenSquare
+  // push();
+  // stroke(255);
+  // rectMode(CENTER);
+  // fill(greenSquare.fill.r, greenSquare.fill.g, greenSquare.fill.b);
+  // rect(field.x + field.size / 2 + 100, greenSquare.y, greenSquare.size);
+  // pop();
+}
 
-  //redSquare
-  push();
-  noStroke();
-  rectMode(CENTER);
-  fill(redSquare.fill.r, redSquare.fill.g, redSquare.fill.b);
-  rect(field.x + field.size / 2 + 100, redSquare.y, redSquare.size);
-  pop();
+// function mouseInsideObject() {
+//   //Get dist b/w mouse and shape
+//   let d = dist(mouseX, mouseY, redCircle.x, redCircle.y);
+//   if (d < redCircle.size / 2) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+//
+// function objectInsideField() {
+//   //Get dist b/w object and field
+//   let d = dist(redCircle.x, redCircle.y, field.x, field.y);
+//   if (d < field.size / 2) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+//
+// function drag() {
+//   if (redCircle.isBeingDragged) {
+//     //Drag object
+//     redCircle.x = mouseX + redCircle.offsetX;
+//     redCircle.x = constrain(mouseX, 0, width);
+//     redCircle.y = mouseY + redCircle.offsetY;
+//     redCircle.y = constrain(mouseY, 0, height);
+//   }
+// }
 
-  //greenSquare
-  push();
-  noStroke();
-  rectMode(CENTER);
-  fill(greenSquare.fill.r, greenSquare.fill.g, greenSquare.fill.b);
-  rect(field.x + field.size / 2 + 100, greenSquare.y, greenSquare.size);
-  pop();
+function mousePressed() {
+  for (let i = 0; i < shapes.length; i++) {
+    let shape = shapes[i];
+
+    if (shape.mouseInsideObject()) {
+      shape.isBeingDragged = true;
+    }
+  }
+}
+
+function mouseReleased() {
+  for (let i = 0; i < shapes.length; i++) {
+    let shape = shapes[i];
+
+    if (shape.isBeingDragged) {
+      shape.isBeingDragged = false;
+    }
+  }
 }
