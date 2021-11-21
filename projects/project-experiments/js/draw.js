18e8c6f0ec94f309c78f47e8386f0f036d776c8e
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2021-11-20 22:00:15 -0500

    drawing program works

--- projects/project-experiments/js/draw.js
+++ projects/project-experiments/js/draw.js
@@ -2,24 +2,23 @@
 
 let keyD = 68;
 
+let textbox = {
+  x: undefined,
+  y: undefined,
+  width: 300,
+  height: 150,
+}
+
+let state = `drawing`;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  textbox.x = width - 300;
+  textbox.y = height - 200;
 }
 
 function draw() {
-  push();
-  textSize(50);
-  textAlign(CENTER);
-  fill(0);
-  text(`Draw what you love`, width/2, 200);
-  pop();
-
-  push();
-  textSize(30);
-  textAlign(CENTER);
-  fill(0);
-  text(`Press D to clear canvas.`, width/2, 250);
-  pop();
 
   // push();
   // textSize(30);
@@ -35,6 +34,17 @@ function draw() {
   // text(`Cmd + shift + 4 to save on Mac.`, width/2, 280);
   // pop();
 
+  if (state === `drawing`) {
+    drawing();
+  } else if (state === `final`) {
+    final();
+  }
+}
+
+function drawing() {
+
+  //HANDLEINPUT FUNCTION
+  //Draw a line
   if (mouseIsPressed) {
     push();
     strokeWeight(2);
@@ -42,8 +52,50 @@ function draw() {
     line(pmouseX, pmouseY, mouseX, mouseY)
     pop();
   }
-
+  //Clear canvas
   if (keyIsDown(keyD)) {
     clear();
+  }
+
+  //instructions
+  push();
+  textSize(50);
+  textAlign(CENTER);
+  fill(0);
+  text(`Draw what you love`, width / 2, 200);
+  pop();
+
+  push();
+  textSize(30);
+  textAlign(CENTER);
+  fill(0);
+  text(`Press D to clear canvas.`, width / 2, 250);
+  pop();
+
+  //TEXTbOX
+  push();
+  rectMode(CENTER);
+  fill(212);
+  rect(textbox.x, textbox.y, textbox.width, textbox.height);
+  pop();
+
+  //proceed text
+  push();
+  textSize(30);
+  textAlign(CENTER);
+  text(`Click here once \nYou're done!`, width - 300, height - 200);
+  pop();
+}
+
+function final() {
+  background(212);
+}
+
+function mousePressed() {
+  if (mouseX > textbox.x - textbox.width / 2 &&
+    mouseX < textbox.x + textbox.width / 2 &&
+    mouseY > textbox.y - textbox.height / 2 &&
+    mouseY < textbox.y + textbox.height / 2) {
+    state = `final`;
   }
 }
